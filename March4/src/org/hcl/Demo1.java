5213d744478ccc5587ebc3aac256bdc66fbe6920
navee <navee@DESKTOP-J7V7KI8>
2022-03-08 23:25:37 +0530

    test

--- March4/src/org/hcl/Demo1.java
+++ March4/src/org/hcl/Demo1.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Demo1 {
 	public static void main(String[] args) throws InterruptedException {
@@ -12,12 +13,22 @@ public class Demo1 {
 	
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\navee\\OneDrive\\Desktop\\March4\\driver\\chromedriver.exe");
 	WebDriver driver =new ChromeDriver();
-	driver.get(" https://www.redbus.in/");
+	driver.get("http://demo.automationtesting.in/Register.html");
 	driver.manage().window().maximize();
-	WebElement findElement = driver.findElement(By.id("sign-in-icon-down"));
-	 findElement.click();
-	 WebElement findElement2 = driver.findElement(By.id("signInLink"));
-	 findElement2.click();
+	WebElement btnYear = driver.findElement(By.id("yearbox"));
+	
+	
+	Select  s =new Select(btnYear);
+	s.selectByValue("1916");////select[@placeholder='Month']
+//	WebElement findElement = driver.findElement(By.xpath("(//select[@type='text'])[4]"));
+	WebElement findElement = driver.findElement(By.xpath("//select[@placeholder='Month']"));
+	Select s1 =new Select(findElement);
+	s.selectByValue("January");
+	
+	
+	// findElement.click();
+	// WebElement findElement2 = driver.findElement(By.id("signInLink"));
+	 //findElement2.click();
 	
 	
 	
